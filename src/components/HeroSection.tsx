df258a49ef73212ad3c9a9a01894667ad29117fc
aiswaryakrishnakumar <aiswaryak401@gmail.com>
2024-08-10 02:04:32 +0530

    Done padding

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import rightArrow from "../images/rightarrow.svg";
-import profileImage from "../images/profile.jpg"; // Update the path to your image
+import profileImage from "../images/profile.jpg"; 
 import { gsap } from "gsap";
 import { componentProps } from "../types";
 
@@ -36,43 +36,47 @@ const HeroSection = ({ openMenu, setOpenMenu, isLoaded }: componentProps) => {
       data-scroll-section
     >
       <Navbar openMenu={openMenu} setOpenMenu={setOpenMenu} />
+  
+      <div className="h-full w-full flex flex-col justify-center py-5 px-2 lg:px-10">
+  <div className="heroSection flex flex-col lg:flex-row justify-center items-center gap-4 lg:gap-8 w-full mt-8 lg:mt-5 mx-auto max-w-screen-lg">
+    <div className="textContainer flex flex-col gap-4 w-full lg:w-1/2 justify-start lg:justify-start lg:mt-0">
+      <h2 className="text-4xl">Web Developer</h2>
+      <h1 className="text-5xl lg:text-6xl font-bold font-[avenir-black]">
+        Aiswarya M K
+      </h1>
+      <p className="text-sm lg:text-base font-[avenir-light]">
+        Hello, I&apos;m Aiswarya, an Indian-based Web Developer with a
+        minimalist design background and a keen sense for learning. Hope to
+        work on your next project.
+      </p>
+      <button
+        className="bg-white text-[#222222] py-3 w-max flex items-center gap-2 px-8"
+        onClick={scrollToProject}
+      >
+        Projects
+        <img src={rightArrow} alt="right arrow" />
+      </button>
+    </div>
 
-      <div className="h-full w-full flex flex-col justify-center py-2 px-4 lg:px-10">
-        <div className="heroSection flex flex-col lg:flex-row gap-4 lg:gap-8 items-center lg:items-start w-full">
-          <div className="textContainer flex flex-col gap-4 lg:w-1/2">
-            <h2 className="text-4xl">Web Developer</h2>
-            <h1 className="text-6xl font-bold font-[avenir-black]">
-              Aiswarya M K
-            </h1>
-            <p className="font-[avenir-light]">
-              Hello, I&apos;m Aiswarya an Indian based Web Developer with a
-              minimalist <br />
-              design background and keen sense for learning. Hope to work with
-              your
-              <br />
-              next project.
-            </p>
+    <div className="imageContainer lg:w-1/2 flex justify-center lg:justify-end lg:mt-0 mt-4">
+      <img
+        src={profileImage}
+        alt="Aiswarya M K"
+        className="w-full max-w-md lg:max-w-full lg:w-auto h-auto object-cover"
+      />
+    </div>
+  </div>
+</div>
 
-            <button
-              className="bg-white text-[#222222] py-3 w-max flex items-center gap-2 px-8"
-              onClick={scrollToProject}
-            >
-              Projects
-              <img src={rightArrow} alt="right arrow" />
-            </button>
-          </div>
-          <div className="imageContainer lg:w-1/2 flex justify-center lg:justify-end mt-4 lg:mt-0">
-            <img
-              src={profileImage}
-              alt="Aiswarya M K"
-              className="w-full max-w-md lg:max-w-full lg:w-auto h-auto object-cover"
-            />
-          </div>
-        </div>
-      </div>
+
       <Footer />
     </div>
   );
+  
+  
+  
+  
+  
 };
 
 export default HeroSection;
